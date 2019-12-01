85b5e17e6bc02a4a759c2187f9dff5ce41f13879
mdaisuke <daisuke.mori@me.com>
2019-12-01 10:28:38 +0900

    next is fibers

--- index.js
+++ index.js
@@ -1,9 +1,77 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom'
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child =>
+                typeof child === "object"
+                ? child
+                : createTextElement(child)
+            ),
+        },
+    }
+}
 
-ReactDOM.render(
-    <div>
-        <h1>hoge</h1>
-    </div>,
-    document.getElementById('root')
+function createTextElement(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    }
+}
+
+function render(element, container) {
+    const dom = 
+        element.type == "TEXT_ELEMENT"
+          ? document.createTextNode("")
+          : document.createElement(element.type)
+    
+    const isProperty = key => key !== "children"
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach(name => {
+            dom[name] = element.props[name]
+        })
+
+    element.props.children.forEach(child =>
+        render(child, dom)
+    )
+
+    container.appendChild(dom)
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+    let shouldYield = false
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        )
+        shouldYield = deadline.timeRemaining() < 1
+    }
+    requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(nextUnitOfWork) {
+    
+}
+
+const Didact = {
+    createElement,
+    render,
+}
+
+/** @jsx Didact.createElement */
+const element = (
+    <div id="foo">
+        <a>bar</a>
+        <b />
+    </div>
 )
+
+Didact.render(element, document.getElementById('root'))
